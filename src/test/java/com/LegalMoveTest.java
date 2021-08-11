be6a151f73e648c602afc8fc88e5f949564cbdb1
kotbi abderrahmane <kotbymo@gmail.com>
2021-08-11 23:14:51 +0100

    implement test cases for legal Move

--- src/test/java/com/LegalMoveTest.java
+++ src/test/java/com/LegalMoveTest.java
@@ -1,11 +1,18 @@
 package com;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class LegalMoveTest {
 
     @Test
     void testCheckMove() {
-        
+        char[][] board = new char[][] { { '.', '.', '.', 'B', '.', '.', '.', '.' },
+                { '.', '.', '.', 'W', '.', '.', '.', '.' }, { '.', '.', '.', 'W', '.', '.', '.', '.' },
+                { '.', '.', '.', 'W', '.', '.', '.', '.' }, { 'W', 'B', 'B', '.', 'W', 'W', 'W', 'B' },
+                { '.', '.', '.', 'B', '.', '.', '.', '.' }, { '.', '.', '.', 'B', '.', '.', '.', '.' },
+                { '.', '.', '.', 'W', '.', '.', '.', '.' } };
+        assertTrue(LegalMove.checkMove(board, 4, 3, 'B'));
     }
 }
