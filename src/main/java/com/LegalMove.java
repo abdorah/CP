ffca34628b56f31f337440ab3cbb90411c9b1692
kotbi abderrahmane <kotbymo@gmail.com>
2021-08-12 21:01:36 +0100

    First attempt, result: failure.

--- src/main/java/com/LegalMove.java
+++ src/main/java/com/LegalMove.java
@@ -1,7 +1,60 @@
 package com;
 
+import java.util.LinkedList;
+import java.util.stream.IntStream;
+
+/**
+ * @author KOTBI Abderrahmane
+ * @version 1.1
+ */
+
 public class LegalMove {
-    public static boolean checkMove(char[][] board, int rMove, int cMove, char color) {
-        return true;
+
+    public static LinkedList<Character> rotateMatrix(char[][] b, int r, int c) {
+        LinkedList<Character> linkedList = new LinkedList<>();
+        IntStream.range(7 - r, 8).forEach(i -> linkedList.add(b[c][7 - r]));
+        return linkedList;
+    }
+
+    public static boolean checkMoveLeftRight(LinkedList<Character> linkedList, int r, int c, char color) {
+        if (linkedList.isEmpty()) {
+            return false;
+        } else {
+            char l = linkedList.peekLast();
+            while (color == l && linkedList.size() >= 3) {
+                l = linkedList.pollLast();
+            }
+            linkedList.removeFirst();
+            if (linkedList.size() >= 2 && linkedList.poll() != color && !linkedList.contains('.')) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static boolean checkMove(char[][] b, int r, int c, char color) {
+        LinkedList<Character> linkedList = new LinkedList<>();
+        final int row = r;
+        IntStream.range(c, 8).forEach(i -> linkedList.add(b[row][i]));
+        boolean result1 = checkMoveLeftRight(linkedList, r, c, color);
+        int temp;
+        rotateMatrix(b, r, c);
+        temp = 7 - r;
+        r = c;
+        c = temp;
+        boolean result2 = checkMoveLeftRight(linkedList, r, c, color);
+        rotateMatrix(b, r, c);
+        temp = 7 - r;
+        r = c;
+        c = temp;
+        boolean result3 = checkMoveLeftRight(linkedList, r, c, color);
+        rotateMatrix(b, r, c);
+        temp = 7 - r;
+        r = c;
+        c = temp;
+        boolean result4 = checkMoveLeftRight(linkedList, r, c, color);
+        boolean result = (result1 ^ result3) || (result2 ^ result4);
+        return result;
     }
 }
--- src/test/java/com/LegalMoveTest.java
+++ src/test/java/com/LegalMoveTest.java
@@ -1,5 +1,6 @@
 package com;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -13,12 +14,62 @@ import org.junit.jupiter.api.Test;
 public class LegalMoveTest {
 
     @Test
-    void testCheckMove() {
+    void testCheckMove1() {
         char[][] board = new char[][] { { '.', '.', '.', 'B', '.', '.', '.', '.' },
                 { '.', '.', '.', 'W', '.', '.', '.', '.' }, { '.', '.', '.', 'W', '.', '.', '.', '.' },
                 { '.', '.', '.', 'W', '.', '.', '.', '.' }, { 'W', 'B', 'B', '.', 'W', 'W', 'W', 'B' },
                 { '.', '.', '.', 'B', '.', '.', '.', '.' }, { '.', '.', '.', 'B', '.', '.', '.', '.' },
                 { '.', '.', '.', 'W', '.', '.', '.', '.' } };
         assertTrue(LegalMove.checkMove(board, 4, 3, 'B'));
+    }
+
+    @Test
+    void testCheckMove2() {
+        char[][] board = new char[][] { { '.', '.', '.', '.', '.', '.', '.', '.' },
+                { '.', 'B', '.', '.', 'W', '.', '.', '.' }, { '.', '.', 'W', '.', '.', '.', '.', '.' },
+                { '.', '.', '.', 'W', 'B', '.', '.', '.' }, { '.', '.', '.', '.', '.', '.', '.', '.' },
+                { '.', '.', '.', '.', 'B', 'W', '.', '.' }, { '.', '.', '.', '.', '.', '.', 'W', '.' },
+                { '.', '.', '.', '.', '.', '.', '.', 'B' } };
+        assertFalse(LegalMove.checkMove(board, 4, 4, 'W'));
+    }
+
+    @Test
+    void testCheckMove3() {
+        char[][] board = new char[][] { { 'W', 'W', '.', 'B', '.', 'B', 'B', '.' },
+                { 'W', 'B', '.', '.', 'W', 'B', '.', '.' }, { 'B', 'B', 'B', 'B', 'W', 'W', 'B', '.' },
+                { 'W', 'B', '.', '.', 'B', 'B', 'B', '.' }, { 'W', 'W', 'B', '.', 'W', '.', 'B', 'B' },
+                { 'B', '.', 'B', 'W', '.', 'B', '.', '.' }, { '.', 'B', 'B', 'W', 'B', 'B', '.', '.' },
+                { 'B', 'B', 'W', '.', '.', 'B', '.', '.' } };
+        assertTrue(LegalMove.checkMove(board, 7, 4, 'B'));
+    }
+
+    @Test
+    void testCheckMove4() {
+        char[][] board = new char[][] { { '.', '.', '.', '.', 'W', 'B', 'B', 'B' },
+                { '.', '.', '.', '.', '.', '.', '.', 'B' }, { 'W', '.', '.', 'W', '.', '.', 'W', '.' },
+                { 'B', '.', 'B', 'B', 'B', '.', '.', 'W' }, { 'W', '.', '.', 'B', 'W', 'B', 'B', '.' },
+                { 'W', 'B', '.', '.', 'W', 'B', 'B', '.' }, { '.', 'W', 'B', 'B', 'W', 'B', '.', 'W' },
+                { 'B', '.', 'W', 'B', 'W', '.', 'W', '.' } };
+        assertFalse(LegalMove.checkMove(board, 0, 0, 'B'));
+    }
+
+    @Test
+    void testCheckMove5() {
+        char[][] board = new char[][] { { '.', '.', '.', 'B', '.', '.', '.', '.' },
+                { '.', '.', '.', 'W', '.', '.', '.', '.' }, { '.', '.', '.', 'W', '.', '.', '.', '.' },
+                { '.', '.', '.', 'W', '.', '.', '.', '.' }, { 'W', 'B', 'B', '.', 'W', 'W', 'W', 'B' },
+                { '.', '.', '.', 'B', '.', '.', '.', '.' }, { '.', '.', '.', 'B', '.', '.', '.', '.' },
+                { '.', '.', '.', 'W', '.', '.', '.', '.' } };
+        assertTrue(LegalMove.checkMove(board, 4, 3, 'B'));
+    }
+
+    @Test
+    void testCheckMove6() {
+        char[][] board = new char[][] { { '.', '.', 'W', '.', 'B', 'W', 'W', 'B' },
+                { 'B', 'W', '.', 'W', '.', 'W', 'B', 'B' }, { '.', 'W', 'B', 'W', 'W', '.', 'W', 'W' },
+                { 'W', 'W', '.', 'W', '.', '.', 'B', 'B' }, { 'B', 'W', 'B', 'B', 'W', 'W', 'B', '.' },
+                { 'W', '.', 'W', '.', '.', 'B', 'W', 'W' }, { 'B', '.', 'B', 'B', '.', '.', 'B', 'B' },
+                { '.', 'W', '.', 'W', '.', 'W', '.', 'W' } };
+        assertTrue(LegalMove.checkMove(board, 5, 4, 'W'));
     }
 }
