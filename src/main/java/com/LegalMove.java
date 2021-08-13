3a1fd909efaa4025801b1908b8b5aa17215b8995
kotbi abderrahmane <kotbymo@gmail.com>
2021-08-13 20:43:09 +0100

    add full directions turning.

--- src/main/java/com/LegalMove.java
+++ src/main/java/com/LegalMove.java
@@ -10,22 +10,47 @@ import java.util.stream.IntStream;
 
 public class LegalMove {
 
-    public static LinkedList<Character> rotateMatrix(char[][] b, int r, int c) {
+    public static LinkedList<Character> rotateMatrix(char[][] b, int r, int c, int round) {
         LinkedList<Character> linkedList = new LinkedList<>();
-        IntStream.range(7 - r, 8).forEach(i -> linkedList.add(b[c][7 - r]));
+        switch (round) {
+            case 0:
+                IntStream.range(c, 8).forEach(i -> linkedList.add(b[r][i]));
+                break;
+            case 1:
+                IntStream.range(7 - r, 8).forEach(i -> linkedList.add(b[7 - i][c]));
+                break;
+            case 2:
+                IntStream.range(7 - c, 8).forEach(i -> linkedList.add(b[r][7 - i]));
+                break;
+            case 3:
+                IntStream.range(r, 8).forEach(i -> linkedList.add(b[i][c]));
+                break;
+            case 4:
+                IntStream.range(0, Math.min(r + 1, 8 - c)).forEach(i -> linkedList.add(b[r - i][c + i]));
+                break;
+            case 5:
+                IntStream.range(0, Math.min(r + 1, c + 1)).forEach(i -> linkedList.add(b[r - i][c - i]));
+                break;
+            case 6:
+                IntStream.range(0, Math.min(8 - r, c + 1)).forEach(i -> linkedList.add(b[r + i][c - i]));
+                break;
+            case 7:
+                IntStream.range(0, Math.min(8 - r, 8 - c)).forEach(i -> linkedList.add(b[r + i][c + i]));
+                break;
+        }
         return linkedList;
     }
 
     public static boolean checkMoveLeftRight(LinkedList<Character> linkedList, int r, int c, char color) {
-        if (linkedList.isEmpty()) {
+        if (linkedList.size() < 3) {
             return false;
         } else {
             char l = linkedList.peekLast();
-            while (color == l && linkedList.size() >= 3) {
+            while ((l == '.' || color == l) && linkedList.size() >= 3) {
                 l = linkedList.pollLast();
             }
             linkedList.removeFirst();
-            if (linkedList.size() >= 2 && linkedList.poll() != color && !linkedList.contains('.')) {
+            if (linkedList.size() >= 2 && !linkedList.contains('.')) {
                 return true;
             } else {
                 return false;
@@ -34,27 +59,15 @@ public class LegalMove {
     }
 
     public static boolean checkMove(char[][] b, int r, int c, char color) {
-        LinkedList<Character> linkedList = new LinkedList<>();
-        final int row = r;
-        IntStream.range(c, 8).forEach(i -> linkedList.add(b[row][i]));
-        boolean result1 = checkMoveLeftRight(linkedList, r, c, color);
-        int temp;
-        rotateMatrix(b, r, c);
-        temp = 7 - r;
-        r = c;
-        c = temp;
-        boolean result2 = checkMoveLeftRight(linkedList, r, c, color);
-        rotateMatrix(b, r, c);
-        temp = 7 - r;
-        r = c;
-        c = temp;
-        boolean result3 = checkMoveLeftRight(linkedList, r, c, color);
-        rotateMatrix(b, r, c);
-        temp = 7 - r;
-        r = c;
-        c = temp;
-        boolean result4 = checkMoveLeftRight(linkedList, r, c, color);
-        boolean result = (result1 ^ result3) || (result2 ^ result4);
+        boolean result1 = checkMoveLeftRight(rotateMatrix(b, r, c, 0), r, c, color);
+        boolean result2 = checkMoveLeftRight(rotateMatrix(b, r, c, 1), r, c, color);
+        boolean result3 = checkMoveLeftRight(rotateMatrix(b, r, c, 2), r, c, color);
+        boolean result4 = checkMoveLeftRight(rotateMatrix(b, r, c, 3), r, c, color);
+        boolean result5 = checkMoveLeftRight(rotateMatrix(b, r, c, 4), r, c, color);
+        boolean result6 = checkMoveLeftRight(rotateMatrix(b, r, c, 5), r, c, color);
+        boolean result7 = checkMoveLeftRight(rotateMatrix(b, r, c, 6), r, c, color);
+        boolean result8 = checkMoveLeftRight(rotateMatrix(b, r, c, 7), r, c, color);
+        boolean result = (result1 ^ result3) || (result2 ^ result4) || (result5 ^ result7) || (result6 ^ result8);
         return result;
     }
 }
