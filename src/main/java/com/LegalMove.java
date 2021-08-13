a5268cdf8983526daac96d72708a53fb1988fa4e
kotbi abderrahmane <kotbymo@gmail.com>
2021-08-14 00:01:10 +0100

    final correct result: legal move closed.

--- src/main/java/com/LegalMove.java
+++ src/main/java/com/LegalMove.java
@@ -10,6 +10,15 @@ import java.util.stream.IntStream;
 
 public class LegalMove {
 
+    /**
+     * @param b
+     * @param r
+     * @param c
+     * @param round
+     * @return linkedList for a selected direction in the matrix that start from the
+     *         point(r,c) till the end of the matrix following on of the eight
+     *         directions
+     */
     public static LinkedList<Character> rotateMatrix(char[][] b, int r, int c, int round) {
         LinkedList<Character> linkedList = new LinkedList<>();
         switch (round) {
@@ -41,7 +50,14 @@ public class LegalMove {
         return linkedList;
     }
 
-    public static boolean checkMoveLeftRight(LinkedList<Character> linkedList, int r, int c, char color) {
+    /**
+     * @param linkedList
+     * @param r
+     * @param c
+     * @param color
+     * @return boolean that checks if the move is legal for one direction
+     */
+    public static boolean checkMoveOneDirection(LinkedList<Character> linkedList, int r, int c, char color) {
         if (linkedList.size() < 3) {
             return false;
         } else {
@@ -61,16 +77,23 @@ public class LegalMove {
         }
     }
 
+    /**
+     * @param b
+     * @param r
+     * @param c
+     * @param color
+     * @return boolean that checks if the move is legal for all the eight directions
+     */
     public static boolean checkMove(char[][] b, int r, int c, char color) {
-        boolean result0 = checkMoveLeftRight(rotateMatrix(b, r, c, 0), r, c, color);
-        boolean result1 = checkMoveLeftRight(rotateMatrix(b, r, c, 1), r, c, color);
-        boolean result2 = checkMoveLeftRight(rotateMatrix(b, r, c, 2), r, c, color);
-        boolean result3 = checkMoveLeftRight(rotateMatrix(b, r, c, 3), r, c, color);
-        boolean result4 = checkMoveLeftRight(rotateMatrix(b, r, c, 4), r, c, color);
-        boolean result5 = checkMoveLeftRight(rotateMatrix(b, r, c, 5), r, c, color);
-        boolean result6 = checkMoveLeftRight(rotateMatrix(b, r, c, 6), r, c, color);
-        boolean result7 = checkMoveLeftRight(rotateMatrix(b, r, c, 7), r, c, color);
-        boolean result = (result0 ^ result2) || (result1 ^ result3) || (result4 ^ result6) || (result5 ^ result7);
+        boolean result0 = checkMoveOneDirection(rotateMatrix(b, r, c, 0), r, c, color);
+        boolean result1 = checkMoveOneDirection(rotateMatrix(b, r, c, 1), r, c, color);
+        boolean result2 = checkMoveOneDirection(rotateMatrix(b, r, c, 2), r, c, color);
+        boolean result3 = checkMoveOneDirection(rotateMatrix(b, r, c, 3), r, c, color);
+        boolean result4 = checkMoveOneDirection(rotateMatrix(b, r, c, 4), r, c, color);
+        boolean result5 = checkMoveOneDirection(rotateMatrix(b, r, c, 5), r, c, color);
+        boolean result6 = checkMoveOneDirection(rotateMatrix(b, r, c, 6), r, c, color);
+        boolean result7 = checkMoveOneDirection(rotateMatrix(b, r, c, 7), r, c, color);
+        boolean result = result0 || result2 || result1 || result3 || result4 || result6 || result5 || result7;
         return result;
     }
 }
