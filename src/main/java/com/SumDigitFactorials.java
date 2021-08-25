1aa8733fa2467a23e6edde3b82d36fb18b526942
kotbi abderrahmane <kotbymo@gmail.com>
2021-08-25 20:11:28 +0100

    add new hacker rank problem; sum digit factorials

---
+++ src/main/java/com/SumDigitFactorials.java
@@ -0,0 +1,37 @@
+package com;
+
+import java.util.HashMap;
+import java.util.stream.LongStream;
+
+public class SumDigitFactorials {
+
+    public static long sFunction(long number) {
+        long digit = 0;
+        long sum = 0;
+        while (number > 0) {
+            digit = number % 10;
+            sum = sum + digit;
+            number = number / 10;
+        }
+        return sum;
+    }
+
+    public static long fFunction(long number) {
+        long digit = 0;
+        long sum = 0;
+        while (number > 0) {
+            digit = number % 10;
+            sum = sum + LongStream.rangeClosed( 1, digit ).reduce(1, ( long a, long b ) -> a * b);
+            number = number / 10;
+        }
+        return sum;
+    }
+
+    public static long gFunction(long number) {
+        return 0;
+    }
+
+    public static long sumSG(HashMap<Long, Long> numbers, long queue) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/SumDigitFactorialsTest.java
@@ -0,0 +1,10 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+
+public class SumDigitFactorialsTest {
+    @Test
+    void testsumSG() {
+        
+    }
+}
