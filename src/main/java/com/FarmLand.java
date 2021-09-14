48cb006fd50140739174563e7f7d17d91e332bf2
kotbi abderrahmane <kotbymo@gmail.com>
2021-09-14 20:42:01 +0100

    Limited edition of FarmLand algorithm(only square)

--- src/main/java/com/FarmLand.java
+++ src/main/java/com/FarmLand.java
@@ -1,6 +1,8 @@
 package com;
 
+import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * @author KOTBI Abderrahmane
@@ -9,32 +11,60 @@ import java.util.ArrayList;
 
 public class FarmLand {
 
+    int i;
+    int j;
+    int dimension;
+
+    public FarmLand() {
+    }
+
+    public FarmLand(int i, int j, int dimension) {
+        this.i = i;
+        this.j = j;
+        this.dimension = dimension;
+    }
+
     /**
      * @param land
      * @return matrix of farm lands
      */
     public static int[][] findFarmland(int[][] land) {
-        int m = land.length;
-        int n = land[0].length;
-        var result = new ArrayList<int[]>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (land[i][j] == 0)
-                    continue;
-                int c1 = j;
-                while (c1 < n && land[i][c1] == 1)
-                    c1++;
-                int r2 = i;
-                while (r2 < m && land[r2][j] == 1)
-                    r2++;
-                c1 = c1 == 0 ? c1 : c1 - 1;
-                r2 = r2 == 0 ? r2 : r2 - 1;
-                result.add(new int[] { i, j, r2, c1 });
-                for (int k = i; k <= r2; k++)
-                    for (int l = j; l <= c1; l++)
-                        land[k][l] = 0;
+        if (Arrays.stream(land).map(e -> Arrays.stream(e).sum()).reduce((a, b) -> a + b).get() == 0) {
+            return new int[][] {};
+        }
+        if (land.length == 1 && Arrays.stream(land[0]).sum() == 1) {
+            return new int[][] { { 0, 0, 0, 0 } };
+        }
+        int dimension;
+        boolean done, actual;
+        List<FarmLand> farms = new ArrayList<FarmLand>();
+        for (int i = 0; i < land.length; i = i + dimension) {
+            dimension = 0;
+            done = false;
+            columnsLoop: for (int j = 0; j < land[0].length; j++) {
+                actual = false;
+                if (land[i][j] == 1) {
+                    ++dimension;
+                    done = Arrays.stream(land[i]).skip(j + 1).sum() == 0;
+                    actual = true;
+                }
+                if (done) {
+                    farms.add(new FarmLand(i, (actual ? j : j - 1), dimension));
+                    break columnsLoop;
+                }
             }
         }
-        return result.toArray(int[][]::new);
+        int[][] result = new int[farms.size()][];
+        for (int k = 0; k < farms.size(); k++) {
+            if (farms.get(k).dimension == 1) {
+                result[k] = new int[] { farms.get(k).i, farms.get(k).j, farms.get(k).i, farms.get(k).j };
+            } else {
+                result[k] = new int[] { farms.get(k).i,
+                    farms.get(k).j - farms.get(k).dimension + 1,
+                    farms.get(k).i + farms.get(k).dimension - 1,
+                    farms.get(k).j};
+            }
+        }
+        return result;
     }
 }
