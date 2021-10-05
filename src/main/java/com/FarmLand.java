6337c86e1915604853cc266056df74e1233946f8
kotbi abderrahmane <kotbymo@gmail.com>
2021-10-05 17:35:19 +0100

    Full version fo FarmLand Algorithm

--- src/main/java/com/FarmLand.java
+++ src/main/java/com/FarmLand.java
@@ -11,60 +11,36 @@ import java.util.Arrays;
 
 public class FarmLand {
 
-    int i;
-    int j;
-    int dimension;
-
-    public FarmLand() {
-    }
-
-    public FarmLand(int i, int j, int dimension) {
-        this.i = i;
-        this.j = j;
-        this.dimension = dimension;
-    }
-
     /**
      * @param land
      * @return matrix of farm lands
      */
     public static int[][] findFarmland(int[][] land) {
-        if (Arrays.stream(land).map(e -> Arrays.stream(e).sum()).reduce((a, b) -> a + b).get() == 0) {
-            return new int[][] {};
-        }
-        if (land.length == 1 && Arrays.stream(land[0]).sum() == 1) {
-            return new int[][] { { 0, 0, 0, 0 } };
-        }
-        int dimension;
-        boolean done, actual;
-        List<FarmLand> farms = new ArrayList<FarmLand>();
-        for (int i = 0; i < land.length; i = i + dimension) {
-            dimension = 0;
-            done = false;
-            columnsLoop: for (int j = 0; j < land[0].length; j++) {
-                actual = false;
+        var result = new ArrayList<int[]>();
+        int row = land.length;
+        int column = land[0].length;
+
+        for (int i = 0; i < row; ++i)
+            for (int j = 0; j < column; ++j)
                 if (land[i][j] == 1) {
-                    ++dimension;
-                    done = Arrays.stream(land[i]).skip(j + 1).sum() == 0;
-                    actual = true;
+                    if (i > 0 && land[i - 1][j] == 1)
+                        continue;
+                    if (j > 0 && land[i][j - 1] == 1)
+                        continue;
+                    int[] buffer = new int[4];
+                    buffer[0] = i;
+                    buffer[1] = j;
+                    int x, y;
+                    for (x = i; x < row; ++x)
+                        if (land[x][j] == 0)
+                            break;
+                    for (y = j; y < column; ++y)
+                        if (land[i][y] == 0)
+                            break;
+                    buffer[2] = x - 1;
+                    buffer[3] = y - 1;
+                    result.add(buffer);
                 }
-                if (done) {
-                    farms.add(new FarmLand(i, (actual ? j : j - 1), dimension));
-                    break columnsLoop;
-                }
-            }
-        }
-        int[][] result = new int[farms.size()][];
-        for (int k = 0; k < farms.size(); k++) {
-            if (farms.get(k).dimension == 1) {
-                result[k] = new int[] { farms.get(k).i, farms.get(k).j, farms.get(k).i, farms.get(k).j };
-            } else {
-                result[k] = new int[] { farms.get(k).i,
-                    farms.get(k).j - farms.get(k).dimension + 1,
-                    farms.get(k).i + farms.get(k).dimension - 1,
-                    farms.get(k).j};
-            }
-        }
-        return result;
+        return result.stream().toArray(int[][]::new);
     }
 }
--- src/test/java/com/FarmLandTest.java
+++ src/test/java/com/FarmLandTest.java
@@ -13,9 +13,44 @@ import org.junit.jupiter.api.Test;
 public class FarmLandTest {
 
     @Test
-    void testFindFarmland() {
+    void testFindFarmland1() {
         int[][] land = new int[][] {{1,0,0},{0,1,1},{0,1,1}};
         int[][] farms = new int[][]{{0,0,0,0},{1,1,2,2}};
+        assertArrayEquals(farms, FarmLand.findFarmland(land));
+    }
+
+    @Test
+    void testFindFarmland2() {
+        int[][] land = new int[][] {{0}};
+        int[][] farms = new int[][]{};
+        assertArrayEquals(farms, FarmLand.findFarmland(land));
+    }
+
+    @Test
+    void testFindFarmland3() {
+        int[][] land = new int[][] {{0},{0}};
+        int[][] farms = new int[][]{};
+        assertArrayEquals(farms, FarmLand.findFarmland(land));
+    }
+
+    @Test
+    void testFindFarmland4() {
+        int[][] land = new int[][] {{1}};
+        int[][] farms = new int[][]{{0,0,0,0}};
+        assertArrayEquals(farms, FarmLand.findFarmland(land));
+    }
+
+    @Test
+    void testFindFarmland5() {
+        int[][] land = new int[][] {{0,1},{1,0}};
+        int[][] farms = new int[][] {{0,1,0,1},{1,0,1,0}};
+        assertArrayEquals(farms, FarmLand.findFarmland(land));
+    }
+
+    @Test
+    void testFindFarmland6() {
+        int[][] land = new int[][] {{1,1},{0,0}};
+        int[][] farms = new int[][] {{0,0,0,1}};
         assertArrayEquals(farms, FarmLand.findFarmland(land));
     }
 }
