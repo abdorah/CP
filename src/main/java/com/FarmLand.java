05726f6acf05c073524ebeb0dee189dad8cdf153
kotbi abderrahmane <kotbymo@gmail.com>
2021-10-06 14:49:51 +0100

    add contest 60 algorithms

--- src/main/java/com/FarmLand.java
+++ src/main/java/com/FarmLand.java
@@ -14,27 +14,31 @@ public class FarmLand {
      * @return matrix of farm lands
      */
     public static int[][] findFarmland(int[][] land) {
-        int m = land.length;
-        int n = land[0].length;
         var result = new ArrayList<int[]>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (land[i][j] == 0)
-                    continue;
-                int c1 = j;
-                while (c1 < n && land[i][c1] == 1)
-                    c1++;
-                int r2 = i;
-                while (r2 < m && land[r2][j] == 1)
-                    r2++;
-                c1 = c1 == 0 ? c1 : c1 - 1;
-                r2 = r2 == 0 ? r2 : r2 - 1;
-                result.add(new int[] { i, j, r2, c1 });
-                for (int k = i; k <= r2; k++)
-                    for (int l = j; l <= c1; l++)
-                        land[k][l] = 0;
-            }
-        }
-        return result.toArray(int[][]::new);
+        int row = land.length;
+        int column = land[0].length;
+
+        for (int i = 0; i < row; ++i)
+            for (int j = 0; j < column; ++j)
+                if (land[i][j] == 1) {
+                    if (i > 0 && land[i - 1][j] == 1)
+                        continue;
+                    if (j > 0 && land[i][j - 1] == 1)
+                        continue;
+                    int[] buffer = new int[4];
+                    buffer[0] = i;
+                    buffer[1] = j;
+                    int x, y;
+                    for (x = i; x < row; ++x)
+                        if (land[x][j] == 0)
+                            break;
+                    for (y = j; y < column; ++y)
+                        if (land[i][y] == 0)
+                            break;
+                    buffer[2] = x - 1;
+                    buffer[3] = y - 1;
+                    result.add(buffer);
+                }
+        return result.stream().toArray(int[][]::new);
     }
 }
